4d1abf183d5225990c2959cb875104ef51a5cf94
brandon-github <brandonmachado159@gmail.com>
2023-08-08 12:54:48 -0500

    interface para el mapeo de datos de actividades

---
+++ src/interfaces/Activity.ts
@@ -0,0 +1,20 @@
+export type ActivityType = "daily" | "weekly" | "monthly";
+
+export interface Activity {
+  title: string;
+  timeframes: {
+    daily: {
+      current: number;
+      previous: number;
+    },
+    weekly: {
+      current: number;
+      previous: number;
+    },
+    monthly: {
+      current: number;
+      previous: number;
+    }
+  },
+  type?: ActivityType;
+}
