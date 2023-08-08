931042f029b2d23beac641f6c4570fbfa94b6e68
brandon-github <brandonmachado159@gmail.com>
2023-08-08 12:51:48 -0500

    adicion de ruta principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+import HomeView from './views/HomeView'
 
 function App() {
-
   return (
-    <div className="w-screen min-h-screen bg-blue-950">
-      <h1 className="text-3xl text-white text-center py-3">Hello World</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomeView />} />
+    </Routes>
   )
 }
 
