08327ee1841ed5e5d00289a4d67023181e83280f
brandon-github <brandonmachado159@gmail.com>
2023-08-08 07:54:03 -0500

    adicion de archivos de configuracion y realizacion de prueba para uso de tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="w-screen min-h-screen bg-blue-950">
+      <h1 className="text-3xl text-white text-center py-3">Hello World</h1>
+    </div>
   )
 }
 
