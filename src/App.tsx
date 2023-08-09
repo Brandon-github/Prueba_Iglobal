f46d19b94b6f0832c1993381abe0ae287901aacb
brandon-github <brandonmachado159@gmail.com>
2023-08-09 09:51:27 -0500

    modificacion de rutas para listado de usuarios y ver detalles de este

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import HomeView from './views/HomeView'
+import UserListView from "./views/UserListView"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeView />} />
+      <Route path="/" element={<UserListView />} />
+      <Route path="/:name/:picture" element={<HomeView />} />
     </Routes>
   )
 }
