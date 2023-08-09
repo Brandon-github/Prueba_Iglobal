e33d37b84e6f1dd708a259f452746d8dfdc6e8bd
brandon-github <brandonmachado159@gmail.com>
2023-08-09 09:57:29 -0500

    modificacion de clases a personalizadas y adicion de lementos

--- src/layouts/HeaderCard.tsx
+++ src/layouts/HeaderCard.tsx
@@ -1,11 +1,13 @@
 
-const HeaderCard = () => {
+const HeaderCard = ({name, picture}: {name: string, picture: string}) => {
   return (
-    <header className="w-full h-4/6 bg-[var(--bg-terciary)] rounded-xl flex flex-col justify-start p-5 max-sm:flex-row max-sm:gap-x-4 max-sm:h-fit max-sm:p-8">
-      <img className="w-16 h-16 object-cover rounded-full border-solid border-2 border-[var(--text-primary)] mb-5" src="/image-jeremy.png" alt="profile photo" />
+    <header className="w-full h-4/6 bg-custom-terciary rounded-xl flex flex-col justify-start p-5 max-sm:flex-row max-sm:gap-x-4 max-sm:h-fit max-sm:p-8">
+      <img className="w-16 h-16 object-cover rounded-full border-solid border-2 border-custom-primary mb-5" src={`https://randomuser.me/api/portraits/women/${picture}`} alt="profile photo" />
       <div className="flex flex-col">
-        <label className="text-[var(--text-primary)] font-thin text-sm">Report for</label>
-        <span className="text-[var(--text-primary)] text-4xl font-extralight">Jeremy Robson</span>
+        <label className="text-custom-primary font-thin text-sm">Report for</label>
+        <span className="text-custom-primary text-4xl font-extralight">
+          {name.split("-")[0]} {name.split("-")[1]}
+        </span>
       </div>
     </header>
   )
