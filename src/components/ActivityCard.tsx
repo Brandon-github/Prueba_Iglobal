6a80750fcdaf88e408e9bb9fab8e266f2617bd93
brandon-github <brandonmachado159@gmail.com>
2023-08-08 19:10:11 -0500

    adicion de iconos y colores para renderizado de tarjetas

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,22 +1,39 @@
+import { useEffect, useState } from "react";
 import { Activity } from "../interfaces/Activity";
-import { IconEllipsis, IconWork } from './icons/Icon'
+import { IconEllipsis, IconExercise, IconPlay, IconSelfCare, IconSocial, IconStudy, IconWork } from './icons/Icon'
 
-const ActivityCard = ({ title, timeframes, type }: Activity) => {
+const ActivityCard = ({ title, timeframes, type, color, icon }: Activity) => {
+
+
+  const setIcon = (index: number) => {
+    let style = "absolute -top-1 right-3 text-gray-600 opacity-[.15]"
+    let icons = [
+      <IconWork className={style} />,
+      <IconPlay className={style} />,
+      <IconStudy className={style} />,
+      <IconExercise className={style} />,
+      <IconSocial className={style} />,
+      <IconSelfCare className={style} />
+    ]
+
+    return icons[index]
+  } 
+
   return (
-    <div className={`bg-[#ff8b64] relative overflow-hidden rounded-xl max-sm:h-[170px] group`}>
-      <IconWork className="absolute -top-3 right-3" />
-      <div className="w-full h-3/4 bg-[var(--bg-secondary)] group-hover:bg-[var(--bg-secondary-active)] absolute bottom-0 rounded-lg p-5 transition-colors ease-out duration-150">
+    <div className={`bg-custom-card-${color ?? "one"} relative overflow-hidden rounded-xl max-sm:h-[170px] group`}>
+      {setIcon(icon ?? 0)}
+      <div className="w-full h-3/4 bg-custom-secondary group-hover:bg-custom-secondary-active absolute bottom-0 rounded-lg p-5 transition-colors ease-out duration-150">
         <div className="flex justify-between items-center mb-4">
-          <span className="text-white">{title}</span>
+          <span className="text-custom-primary">{title} - {color}</span>
           <button className="w-fit h-fit">
-            <IconEllipsis className="w-fit h-fit fill-[var(--text-secondary)] hover:fill-[var(--text-primary)] transition ease-in-out duration-100" />
+            <IconEllipsis className="w-fit h-fit fill-[var(--text-secondary)] hover:fill-custom-primary transition ease-in-out duration-100" />
           </button>
         </div>
         <div className="flex flex-col max-sm:flex-row max-sm:items-center max-sm:justify-between">
-          <span className="text-[var(--text-primary)] text-4xl font-light">
+          <span className="text-custom-primary text-4xl font-light">
             {timeframes[type ?? "daily"].current}hrs
           </span>
-          <span className="text-[var(--text-primary)] font-extralight">
+          <span className="text-custom-primary font-extralight">
             Last week - {timeframes[type ?? "daily"].previous} hrs
           </span>
         </div>
--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -2,18 +2,28 @@ import { useEffect, useState } from "react"
 import { Activity, ActivityType } from "../interfaces/Activity"
 import activitiesData from '../data/data.json'
 import ActivityCard from "./ActivityCard"
+import { v4 as uuid } from 'uuid'
 
 const ActivityList = ({type}: {type: ActivityType}) => {
   const [activities, setActivities] = useState<Activity[]>([])
 
+  const colors = ["one", "two", "three", "four", "five", "size"]
+  
   useEffect(() => {
     setActivities(activitiesData)
   }, [])
 
   return (
     <div className="w-full h-full grid grid-cols-3 gap-5 max-sm:grid-cols-1">
-      {activities.map(({ title, timeframes }) => (
-        <ActivityCard title={title} timeframes={timeframes} type={type} />
+      {activities.map(({ title, timeframes }, index) => (
+        <ActivityCard 
+          title={title} 
+          timeframes={timeframes} 
+          type={type} 
+          color={colors[index]} 
+          icon={index}
+          key={uuid()}
+        />
       ))}
     </div>
   )
