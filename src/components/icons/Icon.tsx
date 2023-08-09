bb42247c3df5a65b06beb69a70c8076d0c8b9ce8
brandon-github <brandonmachado159@gmail.com>
2023-08-08 19:09:10 -0500

    componentes para uso de iconos

---
+++ src/components/icons/Icon.tsx
@@ -0,0 +1,17 @@
+import IconEllipsis from "./IconEllipsis";
+import IconExercise from "./IconExercise";
+import IconPlay from "./IconPlay";
+import IconSelfCare from "./IconSelfCare";
+import IconSocial from "./IconSocial";
+import IconStudy from "./IconStudy";
+import IconWork from "./IconWork";
+
+export {
+  IconEllipsis,
+  IconExercise,
+  IconPlay,
+  IconSelfCare,
+  IconSocial,
+  IconStudy,
+  IconWork
+}
